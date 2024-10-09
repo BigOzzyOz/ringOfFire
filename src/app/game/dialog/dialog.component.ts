da30995d8b91d699d4f0ac07951d2653e89d7cfe
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-10-10 01:04:07 +0200

    added player

---
+++ src/app/game/dialog/dialog.component.ts
@@ -0,0 +1,52 @@
+import {Component, Inject, ChangeDetectorRef} from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogData } from '../game.component';
+import { Game } from '../../../models/game';
+
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: './dialog.component.html',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogModule,
+  ],
+})
+export class DialogOverviewExampleDialog {
+
+  game: Game = new Game;
+  name:string = '';
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    public cdr: ChangeDetectorRef
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  
+
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,24 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
+import { PlayerComponent } from "../player/player.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { DialogOverviewExampleDialog } from './dialog/dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+
+export interface DialogData {
+  name: string;
+}
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, DialogOverviewExampleDialog, MatDialogModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 
 export class GameComponent {
-turnCardAnimation = false;
+name: string = '';
+turnCardAnimation: boolean = false;
 game: Game;
-imageSrc: string | undefined = "assets/img/cards/gray_back.png";
+imageSrc: string | undefined = "assets/img/cards/purple_back.png";
 currentCard?:{name: string, src: string } = { name: 'test', src: 'test' };
 
 
-constructor() {
+constructor(public dialog: MatDialog) {
   this.game = new Game();
   console.log(this.game);
 }
@@ -40,5 +54,17 @@ turnCard() {
   }, 1450)
 };
 }
+
+openDialog(): void {
+  const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+    data: {name: this.name},
+  });
+
+  dialogRef.afterClosed().subscribe((name: string) => {
+    if (name) {
+      this.game.players.push(name);  
+    }
+  });
  
+}
 }
